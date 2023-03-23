41275201cdfdfde5f1a87b80d8d4c380441e7983
Paulina Olszewska <olszewska.paulina17@gmail.com>
2023-03-23 18:04:55 +0100

    Add new files

---
+++ src/pl/jaknauczycsieprogramowania/Main.java
@@ -0,0 +1,10 @@
+package pl.jaknauczycsieprogramowania;
+
+import pl.jaknauczycsieprogramowania.file.imagefile.GIFImageFile;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Monitor.getMaxHeight());
+    }
+}
---
+++ src/pl/jaknauczycsieprogramowania/usbdevice/USBDevice.java
@@ -0,0 +1,8 @@
+package pl.jaknauczycsieprogramowania.usbdevice;
+
+public interface USBDevice {
+    boolean connect();
+    boolean disconnect();
+    String getName();
+
+}
